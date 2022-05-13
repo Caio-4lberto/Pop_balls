26c3a25c2e8438cafd07de1d63dff322d1d03d36
Caio-4lberto <caioalbertodasilva@gmail.com>
2022-05-13 16:41:45 -0300

    Alter position of the balls

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function addBall() {
     var ball = document.createElement("div");
     ball.setAttribute("class", "ball");
 
-    var p1 = Math.floor(Math.random() * 500);
-    var p2 = Math.floor(Math.random() * 400);
+    var p1 = Math.floor(Math.random() * 100);
+    var p2 = Math.floor(Math.random() * 100);
 
     var color1 = Math.floor(Math.random() * 255);
     var color2 = Math.floor(Math.random() * 255);
     var color3 = Math.floor(Math.random() * 255);
     
-    ball.setAttribute("style", "left:"+p1+"px;top:"+p2+"px; background-color: rgb("+ color1 +","+ color2 +","+ color3 +");");
+    ball.setAttribute("style", "left:"+p1+"vw;top:"+p2+"vh; background-color: rgb("+ color1 +","+ color2 +","+ color3 +");");
     ball.setAttribute("onclick", "estourar(this)");
 
     document.body.appendChild(ball);
